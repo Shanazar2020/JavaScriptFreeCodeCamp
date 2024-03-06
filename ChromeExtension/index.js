29246489aa0a9a265443c401279985018cf227d2
Shanazar <snizamaddinow@gmail.com>
2024-03-07 02:06:34 +0500

    improvments in the extension. saves texts.

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,45 +1,102 @@
-let myLeads = [];
-
+const inputHead = document.getElementById('input-head');
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
-if (leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage;
-    renderLeads();
+
+let myLeads = [];
+
+function saveToLocalStorage() {
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
 }
 
-inputBtn.addEventListener('click', () => {
-    if (inputEl.value === ''){
-        return
+function saveInput() {
+    if (inputEl.value === '') {
+        return;
     }
 
-    myLeads.push(inputEl.value);
+    const item = { head: inputHead.value, value: inputEl.value };
+    myLeads.push(item);
     inputEl.value = '';
-    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    inputHead.value = '';
 
-    renderLeads();
-});
+    saveToLocalStorage();
+    render(myLeads);
+}
 
-deleteBtn.addEventListener('dblclick', () => {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads();
-});
+function clearLocalStorageAndRender() {
+    setTimeout(() => {
+        localStorage.clear();
+        myLeads = [];
+        render(myLeads);
+    }, 0);
+}
 
-function renderLeads(){
-    let listItems = '';
-    for (let i = 0; i < myLeads.length; i++){
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `;
+
+function copyToClipboard(textToCopy) {
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => console.log('Text copied to clipboard!'))
+        .catch(err => console.error('Failed to copy text:', err));
+}
+
+function handleListClick(event) {
+    const deleteIcon = event.target.closest('.delete-icon');
+    const copyIcon = event.target.closest('.copy-icon');
+
+    if (deleteIcon) {
+        const leadId = parseInt(deleteIcon.dataset.id);
+        myLeads.splice(leadId, 1);
+        saveToLocalStorage();
+        render(myLeads);
+    } else if (copyIcon) {
+        const textToCopy = copyIcon.dataset.text;
+        copyToClipboard(textToCopy);
     }
+}
+
+inputBtn.addEventListener('click', saveInput);
+
+inputHead.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        saveInput();
+    }
+});
+
+inputEl.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        saveInput();
+    }
+});
+
+deleteBtn.addEventListener('dblclick', clearLocalStorageAndRender);
+
+ulEl.addEventListener('click', handleListClick);
+
+function render(leads) {
+    const listItems = leads.map(lead => `
+        <li>
+            <div class="list-item">
+                <button class="icon-button copy-icon" data-text="${lead.value}">
+                    <img class="icon" width="16" height="16" src="copy-icon.svg" alt="Copy">
+                </button>
+                <div class="text-container">
+                    <span class="lead-head">${lead.head}</span>
+                    <span class="lead-value">${lead.value}</span>
+                </div>
+                <button class="icon-button delete-icon" data-id="${leads.indexOf(lead)}">
+                    <img class="icon" width="16" height="16" src="delete-icon.svg" alt="Delete">
+                </button>
+            </div>
+        </li>
+    `).join('');
+
     ulEl.innerHTML = listItems;
 }
 
+// Check local storage on page load
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
